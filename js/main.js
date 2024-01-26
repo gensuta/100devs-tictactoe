f37d399387e29ddecdf169d2d76ef7bb450bf727
barneserella <girlthisismycodingemail@gmail.com>
2024-01-26 16:23:18 -0600

    added new functions

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@
 const body = document.body; // storing our body to get it quicker
 const cells = document.getElementsByClassName('cell'); // storing all the divs with the class name cell
 
-const xClass = 'x';
-const circleClass = 'circle';
 
 let dataCells = []; // this is where our DataCell objects will live
 
@@ -11,17 +9,17 @@ let currentSymbol = "X"; // starting the game with X going first
 
 const restartBtn = document.getElementById('restartButton'); // storing the restart button
 
-restartBtn.addEventListener('click', function(){
-    StartGame(); // restarting the game by running the StartGame function again! It will clear all cells
-})
+restartBtn.addEventListener('click', StartGame); // restarting the game by running the StartGame function again! It will clear all cells
 
-
-const winningMessage = document.getElementById('winningMessage'); // storing the winning msg div
+const winningMessageElement = document.getElementById('winningMessage'); // storing the winning msg div
+const winningMessageText = document.querySelector('[data-winning-message-text]'); // storing the winning msg 
 
 // *FUNCTIONS* //
 
 function StartGame()
 {
+    currentSymbol = 'X';
+
     dataCells = []; // clearing the array just in case
 
     //using ... to turn cells from an HTMLCollection to an array
@@ -31,6 +29,7 @@ function StartGame()
 
         dataCells.push(newDataCell); // adding our new DataCell to our array! Gotta keep track of them.
     });
+    winningMessageElement.classList.remove('show');
 }
 
 function ChangePlayer()
@@ -40,6 +39,97 @@ function ChangePlayer()
     currentSymbol = (currentSymbol == "X") ? "O" : "X";
 }
 
+// Check for Win / Tie
+function CheckForWin() {
+
+    const player1Moves = dataCells.filter(cell => cell.symbol === 'X');
+    const player2Moves = dataCells.filter(cell => cell.symbol === 'O');
+
+    const checkPlayerWin = (moves) => {
+        
+    // Check for win horizontally
+        for (let i = 0; i < moves.length; i+=3) {
+            if (
+                moves.some(cell => cell.cellElement.dataset.index === i.toString()) &&
+                moves.some(cell => cell.cellElement.dataset.index === (i + 1).toString()) &&
+                moves.some(cell => cell.cellElement.dataset.index === (i + 2).toString())
+                ) {
+                console.log(`${currentSymbol} wins  horizontally!`);
+                return true;
+            }
+        }
+
+        // Check for a win vertically
+        for (let i = 0; i < 3; i++) {
+            if (
+                moves.some(cell => cell.cellElement.dataset.index === i.toString()) &&
+                moves.some(cell => cell.cellElement.dataset.index === (i + 3).toString()) &&
+                moves.some(cell => cell.cellElement.dataset.index === (i + 6).toString())
+                )  {
+                console.log(`${currentSymbol} wins  vertically!`);
+                return true;
+            }
+        }
+
+        // Check for a win diagonally
+        if (
+            moves.some(cell => cell.cellElement.dataset.index === '0') &&
+            moves.some(cell => cell.cellElement.dataset.index === '4') &&
+            moves.some(cell => cell.cellElement.dataset.index === '8')
+        ) {
+            console.log(`${currentSymbol} wins  diagonally!`);
+            return true;
+        }
+
+        if (
+            moves.some(cell => cell.cellElement.dataset.index === '2') &&
+            moves.some(cell => cell.cellElement.dataset.index === '4') &&
+            moves.some(cell => cell.cellElement.dataset.index === '6')
+        ) {
+            console.log(`${currentSymbol} wins  diagonally!`);
+            return true;
+        }else{
+            return false;
+        }
+    };
+
+    const player1Wins = checkPlayerWin(player1Moves);
+    const player2Wins = checkPlayerWin(player2Moves);
+
+    if(player1Wins || player2Wins){
+        
+        endGame(true)
+        
+        console.log(`${currentSymbol}'s Wins!`)
+    }else if(isDraw()){
+        endGame(false)
+        console.log('Draw')
+        // If no win is detected log players moves for debugging purposes only
+        console.log(`X's moves: `, player1Moves.map(cell => cell.cellElement.dataset.index));
+        console.log(`O's moves: `, player2Moves.map(cell => cell.cellElement.dataset.index));
+    }else{
+        // logs for debugging
+        console.log(player1Moves);
+        console.log(player2Moves);
+    }
+};
+
+function endGame(win){
+    if(win){
+        winningMessageText.innerText = `${currentSymbol} Wins!`
+    }else {
+        winningMessageText.innerText = 'Draw!'
+    }
+    winningMessageElement.classList.add('show')
+}
+
+function isDraw(){
+    return dataCells.every(cell =>{
+        return cell.symbol.includes('X') || cell.symbol.includes('O')
+    })
+}
+
+
 
 // *OBJECTS* //
 
@@ -47,13 +137,14 @@ function ChangePlayer()
 class DataCell{
 
     isFilled = false;
+    symbol = ""; // our symbol should be empty! along with it's text
 
     // This constructor is filled with events so that each datacell is being tracked properly
     // Meaning, when a new DataCell is created, it stores one of the divs into it's "cellElement", sets it's symbol to blank
     // and makes sure it's checking if the cellElement is being hovered on, clicked, or if it stopped being hovered on
     constructor(c){
         this.cellElement = c;
-        this.symbol = ""; // our symbol should be empty! along with it's text
+        this.cellElement.dataset.index = c.dataset.index; // Assigning index to the dataset
 
         this.cellElement.addEventListener("mouseover", (event) => {
             if(!this.isFilled)
@@ -72,24 +163,24 @@ class DataCell{
             }
         });
 
-        this.cellElement.addEventListener("mousedown", (event) => {
+        this.cellElement.addEventListener("mousedown", () => {
             if(!this.isFilled)
             {
                 this.SetSymbol(); // since we clicked on a cell, we're setting the text to our current symbol (X or O)
+                CheckForWin();
                 ChangePlayer(); // TODO: Find a way to make this cleaner so it's listening to the object instead of having the object call an outside function -Geneva
             }
         });
     }
-
-    SetSymbol(symbol) // setting the cell to an X or O. Making sure it can't be overwritten by setting isFilled to true
+ 
+    SetSymbol() // setting the cell to an X or O. Making sure it can't be overwritten by setting isFilled to true
     {
         this.cellElement.style.color = "black";
-        this.symbol = symbol;
+        this.symbol = currentSymbol;
         this.cellElement.textContent = currentSymbol;
         this.isFilled = true;
     }
 }
-
 
 // *FUNCTIONS TO BE CALLED WHEN THE PAGE LOADS* //
 
